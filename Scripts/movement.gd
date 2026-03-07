a303c753714a6d7e8283db30ade9cc3da2e5cf1d
DevMa7e1 <thedevma7e1@gmail.com>
2026-03-07 14:18:44 +0200

    Movement code (Claude edition)
    And its better than mine

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -1,54 +1,25 @@
-extends StaticBody2D
+extends RigidBody2D
 
-var gravity_fall_pixel_count = 1
-var cobj = []
-@onready
-var bgs = [$"../bg3", $"../bg1", $"../bg2"]
-@onready
-var bgs2 = [$"../bg3", $"../bg1", $"../bg2"]
+@export var speed: float = 200.0
 
-var gravity_acted = false
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var direction = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Area2D.body_entered.connect(_entered)
-	$Area2D.body_exited.connect(_exited)
+	if Input.is_action_pressed("ui_up") && position.y > -336.0:
+		direction.y -= 10
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 10
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 10
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 10
+	if direction != Vector2.ZERO:
+		state.linear_velocity = direction.normalized() * speed
+	else:
+		state.linear_velocity = Vector2.ZERO
 
-func _entered(body):
-	if(body == bgs[2]):
-		bgs[0].position += Vector2(0, 648*3)
-		bgs2[2] = bgs[0]
-		bgs2[1] = bgs[2]
-		bgs2[0] = bgs[1]
-		bgs = bgs2
-	if(body == bgs[0]):
-		bgs[2].position -= Vector2(0, 648*3)
-		bgs2[0] = bgs[2]
-		bgs2[1] = bgs[0]
-		bgs2[2] = bgs[1]
-		bgs2 = bgs
-	cobj.append(body)
-
-func _exited(body):
-	cobj.erase(body)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var touching = false
-	for i in cobj:
-		if(not "bg" in i.name && i != $"." && i != $"../left" && i != $"../right"):
-			touching = true
-	if !touching:
-		position += Vector2(0, gravity_fall_pixel_count)
-		gravity_acted = true
-	if touching && !gravity_acted:
-		position -= Vector2(0, 1)
-	if Input.is_action_pressed("ui_up"):
-		position += Vector2(0, -5)
-	if Input.is_action_pressed("ui_down") && !touching && not $"../ground/dirt" in cobj:
-		position += Vector2(0, 5)
-		gravity_acted = false
-	if Input.is_action_pressed("ui_left") && not $"../left" in cobj:
-		position += Vector2(-5, 0)
-	if Input.is_action_pressed("ui_right") && not $"../right" in cobj:
-		position += Vector2(5, 0)
+	if position.y < -336.0:
+		$"../left/CollisionShape2D".disabled = true
+	else:
+		$"../left/CollisionShape2D".disabled = false
