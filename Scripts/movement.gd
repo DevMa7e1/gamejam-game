cc852ec6f6f97f75dd9b4072cbc196ece9a425be
DevMa7e1 <thedevma7e1@gmail.com>
2026-03-07 13:00:29 +0200

    Movement code, but, like, it works now

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -2,6 +2,9 @@ extends StaticBody2D
 
 var gravity_fall_pixel_count = 1
 var cobj = []
+@onready
+var bgs = [$"../bg3", $"../bg1", $"../bg2"]
+var bgt
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +12,6 @@ func _ready() -> void:
 	$Area2D.body_exited.connect(_exited)
 
 func _entered(body):
-	print(body)
 	cobj.append(body)
 
 func _exited(body):
@@ -28,3 +30,9 @@ func _process(delta: float) -> void:
 		position += Vector2(0, gravity_fall_pixel_count)
 	if Input.is_action_pressed("ui_up"):
 		position += Vector2(0, -5)
+	if Input.is_action_pressed("ui_down") && not $"../ground/dirt" in cobj:
+		position += Vector2(0, 5)
+	if Input.is_action_pressed("ui_left") && not $"../left" in cobj:
+		position += Vector2(-5, 0)
+	if Input.is_action_pressed("ui_right") && not $"../right" in cobj:
+		position += Vector2(5, 0)
