895ed3d30990774dca180184b6529b1f33ecd72e
N0TM3-1 <copaceanudumitru0@gmail.com>
2026-03-07 16:46:00 +0200

    Modified fish generation script

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,23 +1,49 @@
 extends Node2D
 
+const FISH_SCENE := preload("res://Scenes/fish.tscn")
+const FISH_GROUP := "fish"
+@export var MAX_FISH: int = 20 # CONSTANT
+
 var time = 0.0
 var fish_generate_time = 0
 
+var fishes = 0
+
 var level = 1
 
-# Called when the node enters the scene tree for the first time.
 func gen_fish():
-	var new_fish = preload("res://Scenes/fish.tscn").instantiate()
+	var new_fish = FISH_SCENE.instantiate()
 	new_fish.level = randi_range(1, level)
-	new_fish.position.y = randi_range((new_fish.level-1) * 648 - 32, new_fish.level * 648)
-	$".".add_child(new_fish)
+	new_fish.position.x = randi_range((new_fish.level-1) * 1920 + 50, new_fish.level * 1920)
+	new_fish.position.y = randi_range((new_fish.level-1) * 1080 + 50, new_fish.level * 1080)
+	new_fish.add_to_group(FISH_GROUP)
+	add_child(new_fish)
 	fish_generate_time = time + randi_range(10, 30)
-	
+	fishes += 1
+
+func get_all_fish_positions() -> Array[Vector2]:
+	var fish_positions: Array[Vector2] = []
+	for fish in get_tree().get_nodes_in_group(FISH_GROUP):
+		if fish is Node2D:
+			fish_positions.append((fish as Node2D).global_position)
+	return fish_positions
+
+func print_all_fish_positions() -> void:
+	var fish_positions = get_all_fish_positions()
+	for i in range(fish_positions.size()):
+		print("Fish ", i, " position: ", fish_positions[i])
+	if fish_positions.is_empty():
+		print("No fish found")
+	print("Fish count:", fishes)
+
+
 func _ready() -> void:
 	gen_fish()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time += delta
-	if fish_generate_time <= time:
+	if fish_generate_time <= time && fishes < MAX_FISH:
 		gen_fish()
+
+	if Input.is_action_just_pressed("ui_accept"):
+		print_all_fish_positions()
